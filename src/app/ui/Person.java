18f76da4c53b973ed8c2f871918665ae3073dfc9
coolcoolsuperbilly <goutham.kariyadan.mail@gmail.com>
2026-05-07 08:45:35 +0530

    Update Person.java to public

--- src/app/ui/Person.java
+++ src/app/ui/Person.java
@@ -1,6 +1,6 @@
 package app.ui;
 
-abstract class Person {
+public abstract class Person {
     protected String name;
 
     public Person(String name) {
@@ -10,10 +10,8 @@ abstract class Person {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public abstract void register();
 }
